2de8d17a3ccdfb8820d5563a89a0373d22060db4
AlexaneLG <leguern.a@laposte.net>
2019-07-31 10:29:48 +0200

    Add kinect datas to record and create a Replay Controller

---
+++ Avatar/Assets/Scripts/Controller/ReplayController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplayController : MonoBehaviour {
+
+    // read csv file 
+    // then apply the transform positions of the hands and the neck
+
+    public KinectDataReplayer kinectData;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
