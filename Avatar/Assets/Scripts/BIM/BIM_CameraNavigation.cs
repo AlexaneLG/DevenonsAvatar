e7ef6eebcf0c6aa3acead5c0b79f98a04347ae94
Alexane <alexane.lgrn@gmail.com>
2019-09-20 13:09:18 +0200

    Camera rotation fixed

--- Avatar/Assets/Scripts/BIM/BIM_CameraNavigation.cs
+++ Avatar/Assets/Scripts/BIM/BIM_CameraNavigation.cs
@@ -2,29 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BIM_CameraNavigation : MonoBehaviour {
+public class BIM_CameraNavigation : MonoBehaviour
+{
 
     public Transform Center;
     private Transform _gravityCenter;
+    private float orbitDistance = 10f;
 
     // Use this for initialization
     void Start()
     {
         _gravityCenter = new GameObject().transform;
-        Debug.Log("Center pos y : " + Center.position.y);
         _gravityCenter.position = new Vector3(_gravityCenter.position.x, Center.position.y, _gravityCenter.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*_gravityCenter.position = new Vector3(_gravityCenter.position.x, Center.position.y, _gravityCenter.position.z);
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.RotateAround(_gravityCenter.position, Vector3.up, -0.5f);
+            transform.RotateAround(Center.position, Vector3.up, -0.5f);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.RotateAround(_gravityCenter.position, Vector3.up, 0.5f);
+            transform.RotateAround(Center.position, Vector3.up, 0.5f);
+        }
+        transform.LookAt(Center);*/
+    }
+
+    void LateUpdate()
+    {
+        turnAround();
+    }
+
+    private void turnAround()
+    {
+        transform.position = Center.position + (transform.position - Center.position).normalized * orbitDistance;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.RotateAround(Center.position, Vector3.up, -0.5f);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.RotateAround(Center.position, Vector3.up, 0.5f);
         }
         transform.LookAt(Center);
     }
--- Avatar/Assets/Scripts/BIM/ChangeTimeScale.cs
+++ Avatar/Assets/Scripts/BIM/ChangeTimeScale.cs
@@ -1,31 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
-public class ChangeTimeScale : MonoBehaviour {
+public class ChangeTimeScale : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
 
     public void pauseGame()
     {
+        videoPlayer.Pause();
         Time.timeScale = 0f;
     }
 
     public void playGame()
     {
+        videoPlayer.Play();
         Time.timeScale = 1f;
     }
 
     public void slowGame()
     {
+        videoPlayer.playbackSpeed = 0.5f;
         Time.timeScale = 0.5f;
     }
 
     public void speedGame()
     {
+        videoPlayer.playbackSpeed = 1.5f;
         Time.timeScale = 1.5f;
     }
 
-    public void speedGameToNextScenario ()
+    public void speedGameToNextScenario()
     {
+        videoPlayer.playbackSpeed = 1.5f;
         Time.timeScale = 1.5f;
         // stop condition
     }
