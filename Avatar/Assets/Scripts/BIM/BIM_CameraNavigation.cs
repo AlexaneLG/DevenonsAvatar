6efe05fdb03d03363722c3be1ddda3fa0c8a7d3e
AlexaneLG <leguern.a@laposte.net>
2019-10-01 14:52:54 +0200

    Camera navigation

--- Avatar/Assets/Scripts/BIM/BIM_CameraNavigation.cs
+++ Avatar/Assets/Scripts/BIM/BIM_CameraNavigation.cs
@@ -8,6 +8,13 @@ public class BIM_CameraNavigation : MonoBehaviour
     public Transform Center;
     private Transform _gravityCenter;
     private float orbitDistance = 5f;
+    private float _maxOrbitDistance = 10f;
+    private float _minOrbitDistance = 3f;
+
+    private bool _turningLeft = false;
+    private bool _turningRight = false;
+    private bool _zoomingIn = false;
+    private bool _zoomingOut = false;
 
     // Use this for initialization
     void Start()
@@ -35,6 +42,33 @@ public class BIM_CameraNavigation : MonoBehaviour
     {
         //turnAround();
         transform.position = Center.position + (transform.position - Center.position).normalized * orbitDistance;
+
+        if (_turningRight)
+        {
+            transform.RotateAround(Center.position, Vector3.up, -0.5f);
+        }
+
+        if (_turningLeft)
+        {
+            transform.RotateAround(Center.position, Vector3.up, 0.5f);
+        }
+
+        if (_zoomingIn)
+        {
+            if (orbitDistance > _minOrbitDistance)
+            {
+                orbitDistance -= 0.25f;
+            }
+        }
+
+        if (_zoomingOut)
+        {
+            if (orbitDistance < _maxOrbitDistance)
+            {
+                orbitDistance += 0.25f;
+            }
+        }
+
         transform.LookAt(Center);
     }
 
@@ -56,15 +90,42 @@ public class BIM_CameraNavigation : MonoBehaviour
 
     public void TurnLeft()
     {
-        transform.position = Center.position + (transform.position - Center.position).normalized * orbitDistance;
-        transform.RotateAround(Center.position, Vector3.up, 0.5f);
-        transform.LookAt(Center);
+        _turningLeft = true;
+    }
+
+    public void StopTurningLeft()
+    {
+        _turningLeft = false;
     }
 
     public void TurnRight()
     {
-        transform.position = Center.position + (transform.position - Center.position).normalized * orbitDistance;
-        transform.RotateAround(Center.position, Vector3.up, -0.5f);
-        transform.LookAt(Center);
+        _turningRight = true;
+    }
+
+    public void StopTurningRight()
+    {
+        _turningRight = false;
+    }
+
+    public void ZoomIn()
+    {
+        _zoomingIn = true;
+    }
+
+    public void StopZoomIn()
+    {
+        _zoomingIn = false;
+    }
+
+    public void ZoomOut()
+    {
+        _zoomingOut = true;
+    }
+
+    public void StopZoomOut()
+    {
+        _zoomingOut = false;
+
     }
 }
